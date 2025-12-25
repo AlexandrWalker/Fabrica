76d83a0fc7c173d2c222aaef7abc092bc37cbd6a
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-12-26 01:10:28 +0700

    update

--- build/js/main.js
+++ build/js/main.js
@@ -3,5 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const checkEditMode = document.querySelector('.bx-panel-toggle-on') ?? null;
 
 
+  const burgerBtn = document.getElementById('burger-btn');
 
+  burgerBtn.addEventListener('click', function () {
+
+    document.documentElement.classList.toggle('menu--open');
+  })
 });
